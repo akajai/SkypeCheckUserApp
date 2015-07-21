1a44d5c18f796e770bda6e884290b6c6928d12ac
Ajai Antony Kolarikal <akajai@yahoo.com>
2015-07-21 23:44:24 +0530

    SkypeCheckUser
    Filter Skype user from email

---
+++ SkypeCheckUserApp/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace SkypeCheckUserApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxinput = new System.Windows.Forms.TextBox();
+            this.inputBrowse = new System.Windows.Forms.Button();
+            this.textBoxoutputFIleName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxskypeusername = new System.Windows.Forms.TextBox();
+            this.textBoxskypepassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input File Path";
+            // 
+            // textBoxinput
+            // 
+            this.textBoxinput.Location = new System.Drawing.Point(85, 13);
+            this.textBoxinput.Multiline = true;
+            this.textBoxinput.Name = "textBoxinput";
+            this.textBoxinput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxinput.TabIndex = 1;
+            // 
+            // inputBrowse
+            // 
+            this.inputBrowse.Location = new System.Drawing.Point(209, 9);
+            this.inputBrowse.Name = "inputBrowse";
+            this.inputBrowse.Size = new System.Drawing.Size(75, 23);
+            this.inputBrowse.TabIndex = 2;
+            this.inputBrowse.Text = "Browse";
+            this.inputBrowse.UseVisualStyleBackColor = true;
+            this.inputBrowse.Click += new System.EventHandler(this.inputBrowse_Click);
+            // 
+            // textBoxoutputFIleName
+            // 
+            this.textBoxoutputFIleName.Location = new System.Drawing.Point(85, 45);
+            this.textBoxoutputFIleName.Multiline = true;
+            this.textBoxoutputFIleName.Name = "textBoxoutputFIleName";
+            this.textBoxoutputFIleName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxoutputFIleName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "OutPut File Path";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Skype User ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Skype Password";
+            // 
+            // textBoxskypeusername
+            // 
+            this.textBoxskypeusername.Location = new System.Drawing.Point(105, 69);
+            this.textBoxskypeusername.Name = "textBoxskypeusername";
+            this.textBoxskypeusername.Size = new System.Drawing.Size(100, 20);
+            this.textBoxskypeusername.TabIndex = 7;
+            this.textBoxskypeusername.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxskypepassword
+            // 
+            this.textBoxskypepassword.Location = new System.Drawing.Point(106, 96);
+            this.textBoxskypepassword.Name = "textBoxskypepassword";
+            this.textBoxskypepassword.Size = new System.Drawing.Size(100, 20);
+            this.textBoxskypepassword.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(212, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 251);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(292, 22);
+            this.statusStrip1.TabIndex = 10;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxskypepassword);
+            this.Controls.Add(this.textBoxskypeusername);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxoutputFIleName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.inputBrowse);
+            this.Controls.Add(this.textBoxinput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "SkypeCheckUser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxinput;
+        private System.Windows.Forms.Button inputBrowse;
+        private System.Windows.Forms.TextBox textBoxoutputFIleName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxskypeusername;
+        private System.Windows.Forms.TextBox textBoxskypepassword;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+    }
+}
+
---
+++ SkypeCheckUserApp/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using SKYPE4COMLib;
+using System.Diagnostics;
+namespace SkypeCheckUserApp
+{
+   
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxoutputFIleName.Text = "D:\\out.txt";
+            textBoxinput.Text = "D:\\1.txt";
+            textBoxskypeusername.Text = "check_user1";
+            textBoxskypepassword.Text = "check_user1";
+        }
+
+        private void inputBrowse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (File.Exists(openFileDialog1.FileName))
+                {
+                    textBoxinput.Text = openFileDialog1.FileName;
+                }
+                else
+                {
+                    MessageBox.Show("File Not Present");
+                }
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxoutputFIleName.Text.Length == 0)
+            {
+                MessageBox.Show("Output File Name  Is Empty ");
+            }
+            
+            if (textBoxinput.Text.Length == 0)
+            {
+                MessageBox.Show("Input File Name  Is Empty ");
+            }
+            if (File.Exists(textBoxoutputFIleName.Text))
+            {
+                MessageBox.Show("File Already Present ");
+                //return;
+            }
+            try
+            {
+                System.Diagnostics.Process process = new Process();
+                
+                Skype skype = new Skype();
+
+                
+                if (skype.Client.IsRunning)
+                {
+                    Console.WriteLine("Skype is running");
+                    //skype.Client.Start(true, true);
+                }
+                else
+                {
+//                    Console.WriteLine("Skype is not running so starting skype and login in ");
+//                    //process.WaitForInputIdle("skype.exe /username:" + textBoxskypeusername + "/password:" + textBoxskypepassword);
+
+//                    //System.Diagnostics.Process.Start("C:/Program Files/Skype/Phone/Skype.exe"+" /username:" + textBoxskypeusername + "/password:" + textBoxskypepassword);
+//                    process.StartInfo = new ProcessStartInfo();
+
+//                    process.StartInfo.FileName = @"C:\Program Files\Skype\Phone\Skype.exe";
+//                    process.StartInfo.Verb = "Skype.exe";
+//                   process.StartInfo.CreateNoWindow = true;
+//                    process.StartInfo.Domain = "Domain";
+//                    process.EnableRaisingEvents = true;
+//                    process.StartInfo.UseShellExecute = false;
+//                    //process.StartInfo.LoadUserProfile = true;
+//                    //process.StartInfo.Arguments=" /username:" + textBoxskypeusername + "/password:" + textBoxskypepassword;
+//                    //process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+//                    //process.StartInfo.RedirectStandardOutput = true;
+//                    process.StartInfo.UserName = textBoxskypeusername.Text;
+                    
+//                   //// process.StartInfo.Password.Clear();
+//                   // Console.WriteLine("Skype Username");
+
+//                   // //process.StartInfo.Password.AppendChar('c');
+//                   // //process.StartInfo.Password.AppendChar('h');
+//                   // //process.StartInfo.Password.AppendChar('e');
+//                   // //process.StartInfo.Password.AppendChar('c');
+//                   // //process.StartInfo.Password.AppendChar('k');
+//                   // //process.StartInfo.Password.AppendChar('_');
+//                   // //process.StartInfo.Password.AppendChar('u');
+//                   // //process.StartInfo.Password.AppendChar('s');
+//                   // //process.StartInfo.Password.AppendChar('e');
+//                   // //process.StartInfo.Password.AppendChar('r');
+//                   // //process.StartInfo.Password.AppendChar('1');
+
+//                    for (int i = 0; i < textBoxskypepassword.Text.Length; i++)
+//                    {
+//                        process.StartInfo.Password.AppendChar(textBoxskypepassword.Text.ToString()[i]);
+//                    }
+////                    Console.WriteLine("Password " + process.StartInfo.Password.ToString());
+//                    //process.WaitForInputIdle();
+//                    //process.StartInfo.WorkingDirectory = @"C:\Program Files\Skype\Phone";
+//                    Console.WriteLine("Skype Started");
+                }
+                // wait for the client to be connected and ready
+                //skype.Timeout = 1000;
+                
+                
+                //skype.Attach(6, true);
+                skype.Attach(skype.Protocol, false);
+                ////skype.SilentMode.
+                ////
+                Console.WriteLine("Skype Version" + skype.Version);
+                //Console.WriteLine("Enter a skype name to search for: ");
+                //string username = Console.ReadLine();
+                 //Read the file and display it line by line.
+                int counter = 0;
+                string line;
+                List<String> emailids= new List<String>();
+                
+                System.IO.StreamReader file =new System.IO.StreamReader(textBoxinput.Text);
+                while ((line = file.ReadLine()) != null)
+                {
+                 //   Console.WriteLine(line);
+                    emailids.Add(line);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine("emailids Count " + emailids.Count);
+                 Dictionary<string, string> datalist = new Dictionary<string, string>();
+                List<String> validemailids= new List<String>();
+                
+
+                foreach (String emailid in emailids)
+                {
+                    System.IO.StreamWriter outputfile = new System.IO.StreamWriter(textBoxoutputFIleName.Text, true);
+                    foreach (User user in skype.SearchForUsers(emailid))
+                    {
+
+                        Console.WriteLine(emailid + " : " + user.FullName);
+                        validemailids.Add(emailid);
+                        outputfile.WriteLine(emailid + " : " + user.Aliases +" : " + user.DisplayName+ " : " + user.FullName);
+                    }
+                    outputfile.Close();
+                }
+                
+                
+                //System.IO.StreamWriter outputfile = new System.IO.StreamWriter(textBoxoutputFIleName.Text);
+                //for (int j = 0; j < validemailids.Count; j++)
+                //{
+                //    outputfile.WriteLine(validemailids[j].ToString());
+                //}
+                //outputfile.Close();
+
+                
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Excelption " + ex.Message);
+            }
+
+            
+        }
+    }
+}
